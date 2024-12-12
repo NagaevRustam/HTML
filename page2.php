3db63c6e55bab083baf25cc4c510706efd47c180
NagaevRustam <nagaevrustam@yandex.ru>
2024-12-12 10:04:38 +0300

    Change 2 version

--- page2.php
+++ page2.php
@@ -2,16 +2,18 @@
 $_GET['text'] = 'Задание 2: Создание HTML-страницы';
 echo $_GET['text'];
 // $text = urlencode('Задание 2: Создание HTML-страницы');
-$text = $_GET['text'];
+//$text = $_GET['text'];
 
-if (!file_exists(__DIR__ . 'test.txt')) {
-    //create file
-}
-file_put_contents(__DIR__ . 'test.txt', $text);
-//open file
+//if (!file_exists(__DIR__ . 'test.txt')) {
+//    //create file
+//}
+//file_put_contents(__DIR__ . 'test.txt', $text);
+////open file
 header($_SERVER["SERVER_PROTOCOL"] . " 200 OK");
 header("Cache-Control: public");
-header("Content-type: application/txt");
+header("Content-Type: text/html;charset=UTF-8");
+//header("Content-Disposition: attachment; filename=test.txt");
+//header("Content-type: application/txt");
 header("Content-Transfer-Encoding: Binary");
-header("Content-Disposition: attachment; filename=file.txt");
-?>
+header("Content-Disposition: inline");
+//header("Content-Disposition: attachment; filename=file.txt");
